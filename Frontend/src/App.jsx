88c652cac0285d632eb27ca97e87409e5d0ad989
Malik Aryan Haider <aryanhaider076@gmail.com>
2025-02-08 20:28:17 +0500

    NavBar and Slider Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,17 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WelcomePage from "./components/WelcomePage";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-import Home from "./components/Home"; // Import Home component
-
+import Home from "./components/Home";
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/home" element={<Home />} /> {/* Add Home route */}
-      </Routes>
+      <div className="flex">
+        
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<WelcomePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/Home" element={<Home />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 };
--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import NavBar from "./navbar";
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+    <><NavBar /><div className="min-h-screen bg-black text-white flex items-center justify-center">
       <h1 className="text-3xl font-bold">Welcome to the Home Page!</h1>
-    </div>
+    </div></>
   );
 };
 
